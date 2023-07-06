b22aee13b292b7f700231658dd07c744906cd471
jacks <jacks@192.168.0.16>
2023-07-06 22:10:37 +0900

    課題１９再提出

--- HelloWold_ItoSara/src/basicClass/Animal.java
+++ HelloWold_ItoSara/src/basicClass/Animal.java
@@ -9,13 +9,14 @@ public class Animal {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		String animalName = dog.name;
 		System.out.println(animalName);
-		
-		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		int animalNumber = dog.num;
+
+		// Q2：Q4のDogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog dog2 = new Dog(2);
+		int animalNumber = dog2.getNum();
 		System.out.println(animalNumber);
-		
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		String nowData = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s"));
-        System.out.println(nowData);
-    }
+		System.out.println(nowData);
 	}
+}
