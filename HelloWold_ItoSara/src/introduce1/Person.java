954f9fdc0de2c2befff44bd9b26c31076c13b77b
jacks <jacks@192.168.0.16>
2023-08-05 20:18:32 +0900

    課題３０再提出

--- HelloWold_ItoSara/src/introduce1/Person.java
+++ HelloWold_ItoSara/src/introduce1/Person.java
@@ -7,13 +7,19 @@ public class Person {
 	double height;
 	double weight;
 
+	// 人数の合計を保持
+	static int number = 0;
 	// コンストラクタを定義しインスタンスフィールドに値をセット
 	Person(String name, int age, double height, double weight) {
 		this.name = name;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
+		
+		// 人数加算
+		number++;
 	}
+	
 
 	// インスタンスメソッドBMIを定義
 	public double bmi() {
@@ -24,5 +30,4 @@ public class Person {
 		return formattedBMI;
 
 	}
-
 }
