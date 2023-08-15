0b0e4f6e92bd3d10ee09e1eb161f86d100238ab7
jacks <jacks@DESKTOP-L00USC6>
2023-08-15 23:18:56 +0900

    課題３３提出

---
+++ HelloWold_ItoSara/src/introduce3/Person.java
@@ -0,0 +1,39 @@
+package introduce3;
+
+public class Person {
+	public static int count = 0;
+	public String firstName;
+	// 問題１
+	public String lastName;
+	public int age;
+	public double height, weight;
+
+	// 問題２
+	Person(String firstName, String lastName,int age, double height, double weight) {
+		//　問題３
+		Person.count++;
+		this.firstName = firstName;
+		// 問題３
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public String fullName() {
+		return this.firstName + this.lastName;
+	}
+
+	public void print() {
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+
+	public static void printCount() {
+		System.out.println("合計" + Person.count + "人です");
+	}
+}
